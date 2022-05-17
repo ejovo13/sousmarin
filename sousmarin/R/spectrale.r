88563d1d462759d3fbd4cab3ad9b37d146598081
archstation <ejovo13@yahoo.com>
2022-05-17 10:01:40 +0200

    add bibliography

---
+++ sousmarin/R/spectrale.r
@@ -0,0 +1,18 @@
+# we want gaussian covariance
+#
+# C(h) = exp(-|h|^2/a^2)
+#
+# spectral density f(\omega) :
+#
+# f(w) = (a /(2 * sqrt(pi)))
+#
+#
+
+library(pracma)
+
+f_omega <- function(w, a = 1, d = 1) {
+    ((a / (2 * sqrt(pi))) ** d) * exp(- (a ** 2 * abs(w) ** 2) / 4)
+}
+
+# Package sur GitHub
+devtools::install_github("EricMarcon/SpatDiv")
