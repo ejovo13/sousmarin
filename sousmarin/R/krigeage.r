c23deb38390bc0548bc216f1117869409b569d98
Evan Voyles (laptop) <ejovo13@yahoo.com>
2022-05-19 19:09:06 +0200

    add export statements

--- sousmarin/R/krigeage.r
+++ sousmarin/R/krigeage.r
@@ -3,19 +3,23 @@ library(purrr)
 
 n <- 5 # start with 5 x 5 grid
 
+#' @export
 ind_to_coord <- function(index, n) {
     c(floor((index - 1) / n) + 1, mod(index - 1, n) + 1)
 }
 
+#' @export
 coord_to_dist <- function(pos1, pos2) {
     sqrt((pos1[1] - pos2[1]) ** 2 + (pos1[2] - pos2[2]) ** 2)
 }
 
+#' @export
 cov_model <- function(h, a = sqrt(8)) {
     exp(-abs(h) / a)
 }
 
 # i is the row of the Sigma matrix
+#' @export
 cov_get_row <- function(i, n, a = sqrt(8)) {
 
     len_out <- (n * n) - i + 1
@@ -29,6 +33,7 @@ cov_get_row <- function(i, n, a = sqrt(8)) {
     x
 }
 
+#' @export
 get_cov_matrix <- function(n, a = 2) {
 
     n2 <- n * n
