a24a4571633a5ec18e209d008c46f9c1b49c8664
archstation <ejovo13@yahoo.com>
2022-05-20 09:55:54 +0200

    include removed quotes, add samson's part

--- tex/code/gauss_spat_sim.r
+++ tex/code/gauss_spat_sim.r
@@ -12,24 +12,24 @@ n <- 60 # 60 takes about a MINUTE maybe more.
 mu <- rep(0, n * n)
 # sigma <- get_cov_matrix_gauss(n, 2.7, 1)
 # sigma <- get_cov_matrix_exp(n, 6)
-# sigma <- get_cov_matrix_sphere(n, 6)
-sigma <- get_cov_matrix_tent(n, 3, 5)
+sigma <- get_cov_matrix_sphere(n, 6)
+# sigma <- get_cov_matrix_tent(n, 3, 5)
 L <- chol(sigma)
 
 Z <- matrix(c(rmvnorm_chol(1, L, mu)), nrow = n, byrow = TRUE)
 image(Z, col = hcl.colors(5, palette = "plasma"), bty = "n", xaxt = "n", yaxt = "n")
 
 # # let's create a 6 x 6 image of 60x60 sections
-# grid_n <- 2
+grid_n <- 2
 
-# big_image <- matrix(rep(0, grid_n * grid_n * n * n), nrow = n * grid_n)
-# for(i in 1:grid_n) {
-#     for (j in 1:grid_n) {
-#         big_image[((i - 1) * n + 1):(i * n), ((j - 1) * n + 1):(j * n)] = matrix(c(rmvnorm_chol(1, L, mu)), nrow = n, byrow = TRUE)
-#     }
-# }
+big_image <- matrix(rep(0, grid_n * grid_n * n * n), nrow = n * grid_n)
+for(i in 1:grid_n) {
+    for (j in 1:grid_n) {
+        big_image[((i - 1) * n + 1):(i * n), ((j - 1) * n + 1):(j * n)] = matrix(c(rmvnorm_chol(1, L, mu)), nrow = n, byrow = TRUE)
+    }
+}
 
-# image(big_image, col = hcl.colors(5, palette = "viridis"), bty = "n", xaxt = "n", yaxt = "n")
+image(big_image, col = hcl.colors(5, palette = "viridis"), bty = "n", xaxt = "n", yaxt = "n")
 
 # library(tikzDevice)
 
